b4f64da4b65f4e9979ce2265309500a4014bed54
habibb2r <habibhk127@gmail.com>
2024-10-15 23:06:59 +0600

    Formated Manage User

--- src/Page/DashBoard/Admin/ManageUser/TableUsers.jsx
+++ src/Page/DashBoard/Admin/ManageUser/TableUsers.jsx
@@ -8,21 +8,34 @@ const TableUsers = ({ user, index, refetch }) => {
   const [axiosSecure] = useAxiosSecure();
   const handleDelete = (removeUser) => {
     const email = removeUser.email
-    axiosSecure.delete(`/removeuser?email=${email}`)
-    .then(res=>{
-      
-      if(res.data.status){
-        refetch();
-            Swal.fire({
-              position: "center",
-              icon: "success",
-              title: `${removeUser.name} Removed`,
-              showConfirmButton: false,
-              timer: 1500,
-            });
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/removeuser?email=${email}`)
+        .then(res=>{
+          if(res.data.status){
+            refetch();
+                Swal.fire({
+                  position: "center",
+                  icon: "success",
+                  title: `${removeUser.name} Removed`,
+                  showConfirmButton: false,
+                  timer: 1500,
+                });
+          }
+        }
+        )
       }
-    }
-    )
+    });
+    
   };
   const handleMakeAdmin = (user) => {
     console.log(user);
