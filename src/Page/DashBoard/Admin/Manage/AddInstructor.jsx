a772ca489cbe35d15b731d0eb3871793e6e60a37
habibb2r <habibhk127@gmail.com>
2024-10-16 20:13:25 +0600

    Make Instructor Function Done

--- src/Page/DashBoard/Admin/Manage/AddInstructor.jsx
+++ src/Page/DashBoard/Admin/Manage/AddInstructor.jsx
@@ -1,17 +1,46 @@
 import Loading from "../../../Shared/Loading";
 import useGetUser from "../AdminHooks/useGetUser";
 import addasins from '../../../../assets/icons/001-add-user.png'
+import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const AddInstructor = () => {
     const [userList, refetch, loadUsers] = useGetUser();
+    const [axiosSecure] = useAxiosSecure()
     if(loadUsers){
         return <Loading></Loading>
     }
     const filteruser = userList.filter(user => user.role === 'student')
+
+    const handleMakeInstructor = (email) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You want to make instructor!",
+            icon: "info",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Make Instructor!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.patch(`/makeInstructor?email=${email}`)
+                .then(res=>{
+                    if(res.data.status){
+                        refetch()                       
+                        Swal.fire({
+                            title: "Inserted",
+                            text: "Succesfully added as Instructor",
+                            icon: "success"
+                        });
+                    }
+                })
+            }
+          });
+    }
     return (
         <div> 
             <h1 className="font-bold text-3xl text-center py-3">Add Instructor</h1>
-            <div className="grid grid-cols-1 gap-3 px-5 py-3">
+            <div className="grid grid-cols-2 gap-3 px-5 py-3">
                 {filteruser.map(user=> <div className="bg-yellow-600 bg-opacity-25 px-5 py-3 flex justify-between items-center gap-5" key={user._id}>
                     <div className="flex justify-center items-center gap-3">
                         <img className="h-[80px] w-[80px] rounded-2xl shadow-md shadow-primary" src={user.photo} alt="" />
@@ -22,7 +51,7 @@ const AddInstructor = () => {
                     </div>
                     <div className="flex justify-center items-center gap-4">
                         <p className={`font-serif py-2 px-3 bg-accent rounded-2xl`}>{user.role}</p>
-                        <img className="h-[35px]" src={addasins} alt="" />
+                        <img onClick={()=>handleMakeInstructor(user.email)} className="h-[35px]" src={addasins} alt="" />
                     </div>
                 </div>)}
             </div>
